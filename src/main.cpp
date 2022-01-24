0fb4aa09f39335f1f63a8ccb807c3349ba598d5d
Asher Mancinelli <ashermancinelli@gmail.com>
2022-01-24 12:12:35 -0700

    First pass

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,14 @@
 
 #include <cstdlib>
 #include <ghc/filesystem.hpp>
+#include <optional>
 #include <string>
 namespace fs = ghc::filesystem;
+using namespace std::string_literals;
 
 static constexpr std::size_t path_max = 1024;
+static const std::string newline = "\n ⦙";
+static constexpr std::size_t max_dir_len = 40;
 
 static std::string get_machine_name()
 {
@@ -50,14 +54,71 @@ static std::string get_dir()
   }
 }
 
+static std::optional<std::string> get_slurm_string()
+{
+  char* slurm_id = getenv("SLURM_JOB_ID");
+  if (slurm_id != NULL) {
+    return fmt::format("{}job {}",
+                       newline,
+                       fmt::format(fg(fmt::color::rebecca_purple),
+                                   "{}",
+                                   slurm_id));
+  }
+  return std::nullopt;
+}
+
+static std::string get_git_str()
+{
+  char cmd[path_max] = "git rev-parse --abbrev-ref HEAD 2> /dev/null";
+  FILE* fp = popen(cmd, "r");
+  if (!fp) {
+    std::exit(EXIT_FAILURE);
+  }
+
+  char out[path_max];
+  fgets(out, sizeof(out), fp);
+  pclose(fp);
+  for (char* i = out; i != (out + strlen(out)); i++) {
+    if (*i == '\n') {
+      *i = 0;
+      break;
+    }
+  }
+  return std::string{out};
+}
+
+static bool is_git_dir()
+{
+  auto ec = system("git rev-parse 2>/dev/null 1>/dev/null");
+  return (not ec) or fs::exists(fs::path(".git"));
+}
+
 int main()
 {
   const auto mach = get_machine_name();
-  const auto dir = get_dir();
+  auto dir = get_dir();
+  if (dir.size() > max_dir_len) {
+    static const auto sz = dir.size();
+    // Number of characters to keep on each side of the dir
+    static constexpr std::size_t chars_to_keep = 5;
+    const auto start = dir.find("/", 1);
+    const auto last = dir.rfind("/");
+    dir.replace(start + 1, last - start - 1, "..."s);
+  }
   const auto name = get_user_name();
-  fmt::print("{} on {} in {}\n $ ",
+  auto git_str = std::string{""};
+  if (is_git_dir()) {
+    git_str = fmt::format(" on {}",
+                          fmt::format(fg(fmt::color::light_salmon),
+                                      "{}",
+                                      get_git_str()));
+  }
+  const auto slurmid = get_slurm_string();
+  fmt::print("{} on {} in {}{}{}\n ⤷  ",
              fmt::format(fg(fmt::color::cyan), "{}", name),
              fmt::format(fg(fmt::color::yellow), "{}", mach),
-             fmt::format(fg(fmt::color::light_green), "{}", dir));
+             fmt::format(fg(fmt::color::light_green), "{}", dir),
+             git_str,
+             (slurmid.has_value() ? slurmid.value() : ""));
   return 0;
 }
